884c2e21adc8ec5970de3ef65aa94796ca2071f6
Noah Ashworth <noah.ashworth-welsh@snhu.edu>
2023-12-12 19:07:50 -0500

    this for final more stuff

---
+++ pages/forms.py
@@ -0,0 +1,11 @@
+# app/forms.py
+from django import forms
+from .models import Image
+
+
+class ImageForm(forms.ModelForm):
+    body = forms.CharField(widget=forms.Textarea, required=False)
+
+    class Meta:
+        model = Image
+        fields = ("title", "body", "image")
