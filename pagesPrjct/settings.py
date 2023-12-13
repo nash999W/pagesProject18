0712bbaa0cf337da2e067025adeb419dffbcdcfb
Noah Ashworth <noah.ashworth-welsh@snhu.edu>
2023-12-13 13:39:16 -0500

    this for final more stuff

--- pagesPrjct/settings.py
+++ pagesPrjct/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "pages",
-    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +117,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
